d36d277004a9a8ea67216f3a731574dd421a6eca
studentyakse <ivanivanov2020m@yandex.ru>
2021-11-15 09:33:43 +0900

    Практическая работа № 11 Локальная авторизация

---
+++ app/src/main/java/com/example/auth2/MainActivity.java
@@ -0,0 +1,65 @@
+package com.example.auth2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public EditText username;
+    public EditText password;
+    public Button login;
+    public TextView loginLocked;
+    public TextView attempts;
+    public TextView numberOfAttempts;
+
+    int numberOfRemainingLoginAttempts = 3;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        username = (EditText) findViewById(R.id.edit_user);
+        password = (EditText) findViewById(R.id.edit_password);
+        login = (Button) findViewById(R.id.button_login);
+        loginLocked = (TextView) findViewById(R.id.login_locked);
+        attempts = (TextView) findViewById(R.id.attempts);
+        numberOfAttempts = (TextView) findViewById(R.id.number_of_attempts);
+        numberOfAttempts.setText(Integer.toString(numberOfRemainingLoginAttempts));
+
+    }
+
+    public void Login(View view) {
+
+        if (username.getText().toString().equals("admin") &&
+                password.getText().toString().equals("admin")) {
+            Toast.makeText(getApplicationContext(), "Вход выполнен!",Toast.LENGTH_SHORT).show();
+
+            Intent intent = new Intent(MainActivity.this,Second.class);
+            startActivity(intent);
+        }
+
+        else {
+            Toast.makeText(getApplicationContext(), "Неправильные данные!",Toast.LENGTH_SHORT).show();
+            numberOfRemainingLoginAttempts--;
+
+            attempts.setVisibility(View.VISIBLE);
+            numberOfAttempts.setVisibility(View.VISIBLE);
+            numberOfAttempts.setText(Integer.toString(numberOfRemainingLoginAttempts));
+
+            if (numberOfRemainingLoginAttempts == 0) {
+                login.setEnabled(false);
+                loginLocked.setVisibility(View.VISIBLE);
+                loginLocked.setBackgroundColor(Color.RED);
+                loginLocked.setText("Вход заблокирован!!!");
+            }
+        }
+    }
+}
